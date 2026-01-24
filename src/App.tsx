49fbe15f4d5f0dbe19421123bea44bd4d6f777f1
Bulbashok <bulbashok97@gmail.com>
2026-01-24 15:42:07 +0300

    feat: setup basic routing with Catalog and Cart pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import CatalogPage from "./pages/CatalogPage";
+import CartPage from "./pages/CartPage";
 
 function App() {
   return (
-    <>
-      <h1>Store-app</h1>
-    </>
+    <div className="app">
+      <Routes>
+        <Route path="/" element={<CatalogPage />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Routes>
+    </div>
   );
 }
 
